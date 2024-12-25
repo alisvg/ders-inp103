8b892de7abc7e316edb19e8b109b3c21f994d877
alisvg <aliemre.otun@std.yeditepe.edu.tr>
2024-12-25 16:37:30 +0300

    Not yaş py

---
+++ not yaş hesabı.py
@@ -0,0 +1,12 @@
+note = float(input("Notunuz: "))
+yas = int(input("Yaşınız: "))
+
+if note > 80:
+  if 20 <= yas <= 50:
+    print("Einstelle")
+elif note < 80:
+  print("Ablehner")
+else:
+  print("Ablehner")
+  
+
